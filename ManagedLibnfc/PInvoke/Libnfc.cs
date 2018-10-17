d81977ab8efa2a5c8e3d0a255f572d370b7db0f2
kgame <kgamecarter@gmail.com>
2018-10-17 11:59:19 +0800

    wrap nfc_init, nfc_exit

--- ManagedLibnfc/PInvoke/Libnfc.cs
+++ ManagedLibnfc/PInvoke/Libnfc.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace ManagedLibnfc.PInvoke
 {
     public static class Libnfc
     {
+        /// <summary>
+        /// Initialize libnfc.
+        /// This function must be called before calling any other libnfc function
+        /// </summary>
+        /// <param name="context">Output location for NfcContext</param>
+        [DllImport("libnfc", EntryPoint = "nfc_init", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void NfcInit(out IntPtr context);
+
+        /// <summary>
+        /// Deinitialize libnfc.
+        /// Should be called after closing all open devices and before your application terminates.
+        /// </summary>
+        /// <param name="context">The context to deinitialize</param>
+        [DllImport("libnfc", EntryPoint = "nfc_exit", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void NfcExit(IntPtr context);
     }
 }
